5097621b272d4493939c256fa27860ac9388daf3
Erick Silva <106249621+Ericksilvadeoliveira@users.noreply.github.com>
2022-10-22 19:17:40 -0300

    algumas tentativas

--- usual.js
+++ usual.js
@@ -3,21 +3,7 @@ var trocaPaginas = document.querySelector('.troca-paginas');
 var explorar = document.querySelector('.explorar');
 var explorarContent = document.querySelector('.explorar-content');
 
-paginaInicial.addEventListener('click', function() {
-    if(trocaPaginas.style.display === 'block') {
-        trocaPaginas.style.display = 'none';
-    }else {
-        trocaPaginas.style.display = 'block';
-        explorarContent.style.display = 'none';
-    }
-});
-
 explorar.addEventListener('click', function() {
-    if(explorarContent.style.display === 'block') {
-        explorarContent.style.display = 'none';
-    }else {
-        explorarContent.style.display = 'block';
-        trocaPaginas.style.display = 'none';
-    }
-});
+    
+})
 
