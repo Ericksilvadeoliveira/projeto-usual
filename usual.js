2fd33e26e40dbfef2aa0aa4bcf142ab73072e7c7
Erick Silva <106249621+Ericksilvadeoliveira@users.noreply.github.com>
2022-10-28 19:59:58 -0300

    tamanho dos elementos no header

--- usual.js
+++ usual.js
@@ -1,12 +1,2 @@
-var pagInicial = document.querySelector('.pagInicial');
 
-function aparecerNome () {
-    var displayDiv = document.getElementById('home').style.display;
-    if (displayDiv = 'none') {
-        document.getElementById.style.display = 'block';
-    } else {
-        document.getElementById.style.display = 'none';
-    }
-}
-pagInicial.addEventListener('mouseenter', aparecerNome (Event))
 
